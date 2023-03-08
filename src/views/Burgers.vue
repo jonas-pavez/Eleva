5b11e08539956a4dc1af1e60622a3afde87ab637
jonas-pavez <jlportus@hotmail.com>
2023-03-08 10:26:03 -0300

    Subir Proyecto

---
+++ src/views/Burgers.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Completa aquí el desafío</h1>
+    <v-btn>Vuetify</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Burgers',
+}
+</script>
+
+<style>
+</style>
